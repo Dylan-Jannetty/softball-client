6d6453b3518ddac4bcccc543289cf4d47afe02ae
dylan-jannetty <djannetty29@gmail.com>
2018-05-29 21:42:02 -0400

    update team & get teams on load of page
    ui.js:
      started what I thought would remove and append appropriate functions when
      signed up or signed in. Still needs to be resolved.
    
    teamapi.js:
      created a updateTeam AJAX request
    
    teamevents.js:
      made an event and addHandler for onUpdateTeam
    
    teamui.js:
      -switched all messages to show up in dropdown menu
      -created an onUpdateTeamSuccess and onUpdateTeamFailure
    
    all handlebars:
      made titles h4 to shrink
    
    nav.handlebars:
      -added a class nav-message so messages can be displayed in options dropdown
      -add update-team dropdown list
    
    update-team.handlebars:
      -created a form to update teams
      -it can update but all selections must be filled. looking to fix problem
      possibly by preoccupying the selection just need to figure out how.
    
    index.js:
      -added team.Events.onGetTeamNoEvent() so that when page loads the list
      of teams shows
      -have a few things commented out of an attempt to display appropriate things
      at appropriate time.. still needs work.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,97 +2,96 @@
 const store = require('../store')
 
 const signUpSuccess = function () {
-  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed up</div>`)
-  $('#message').css('text-align', 'center')
+  $('.nav-message').html(`<div class="alert alert-success" role="alert">You have succesfully signed up</div>`)
+  $('.nav-message').css('text-align', 'center')
   $('form').trigger('reset')
+  $('.sign-in').remove()
+  $('.sign-up').remove()
   setTimeout(() => {
-    $('#message').html('')
+    $('.nav-message').html('')
   }, 3000
   )
+  // $('.dropdown-menu').append('.change-password')
+  // $('.dropdown-menu').append('.delete-team')
+  // $('.dropdown-menu').append('.create-team')
+  // $('.dropdown-menu').append('.update-team')
+  // $('.dropdown-menu').append('.sign-out')
 }
 
 const signUpFailure = function () {
-  $('#message').html(`<div class="alert alert-danger" role="alert">You have failed to sign up!<br>Your username or email may be already registered.</div>`)
-  $('#message').css('text-align', 'center')
+  $('.nav-message').html(`<div class="alert alert-danger" role="alert">You have failed to sign up!<br>Your username or email may be already registered.</div>`)
+  $('.nav-message').css('text-align', 'center')
   $('form').trigger('reset')
   setTimeout(() => {
-    $('#message').html('')
+    $('.nav-message').html('')
   }, 3000
   )
 }
 
 const signInSuccess = function (data) {
   console.log(data)
-  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed in!</div>`)
-  $('#message').css('text-align', 'center')
+  $('.nav-message').html(`<div class="alert alert-success" role="alert">You have succesfully signed in!</div>`)
+  $('.nav-message').css('text-align', 'center')
   $('form').trigger('reset')
-  $('.modal').modal('hide')
+  // $('.sign-in').remove()
+  // $('.sign-up').remove()
+
+  // const changePasswordHandlebars = require('../templates/change-password.handlebars')
+  // const changePasswordHTML = changePasswordHandlebars()
+  // $('.change-password').append(changePasswordHTML)
+  //
+  // const deleteTeamHandlebars = require('../templates/delete-team.handlebars')
+  // const deleteTeamHTML = deleteTeamHandlebars()
+  // $('.delete-team').append(deleteTeamHTML)
+  //
+  // const createTeamHandlebars = require('../templates/create-team.handlebars')
+  // const createTeamHTML = createTeamHandlebars()
+  // $('.create-team').append(createTeamHTML)
+  //
+  // const updateTeamHandlebars = require('../templates/update-team.handlebars')
+  // const updateTeamHTML = updateTeamHandlebars()
+  // $('.update-team').append(updateTeamHTML)
+  //
+  // const signOutUserHandlebars = require('../templates/sign-out.handlebars')
+  // const signOutUserHTML = signOutUserHandlebars()
+  // $('.sign-out').append(signOutUserHTML)
+
   setTimeout(() => {
-    $('#message').html('')
+    $('.nav-message').html('')
   }, 3000
   )
-
   store.user = data.user
-  // const createFileUploadHandlebars = require('../templates/file-upload/create-file.handlebars')
-  // const createFileUploadHTML = createFileUploadHandlebars()
-  //
-  // const updateFileUploadHandlebars = require('../templates/file-upload/update-file.handlebars')
-  // const updateFileUploadHTML = updateFileUploadHandlebars()
 
-  // const viewFileUploadHandlebars = require('../templates/file-upload/view-file.handlebars')
-  // const viewFileUploadHTML = viewFileUploadHandlebars()
-  //
-  // const deleteFileUploadHandlebars = require('../templates/file-upload/delete-file.handlebars')
-  // const deleteFileUploadHTML = deleteFileUploadHandlebars()
-
-  // const navHandlebars = require('../templates/nav.handlebars')
-  // const navHTML = navHandlebars({username: store.user.username})
-
-  // const scrollFileHandlebars = require('../templates/scrollfile.handlebars')
-  // const scrollFileHTML = scrollFileHandlebars()
-
-  // const containerHandlebars = require('../templates/container.handlebars')
-  // const containerHTML = containerHandlebars()
-
-  // $('body').on('click', '.file-tabs a', function (e) {
-  //   e.preventDefault()
-  //   $(this).tab('show')
-  // })
-
-  const signOutUserHandlebars = require('../templates/sign-out.handlebars')
-  const signOutUserHTML = signOutUserHandlebars()
-
-  $('body').append(signOutUserHTML)
   // $('.jumbotron').remove()
   // // console.log(store.user)
 }
 
 const signInFailure = function () {
-  $('#message').html(`<div class="alert alert-danger" role="alert">You have failed sign in!</div>`)
-  $('#message').css('text-align', 'center')
+  $('.nav-message').html(`<div class="alert alert-danger" role="alert">You have failed sign in!</div>`)
+  $('.nav-message').css('text-align', 'center')
   $('form').trigger('reset')
   setTimeout(() => {
-    $('#message').html('')
+    $('.nav-message').html('')
   }, 3000
   )
 }
 
 const changePasswordSuccess = function () {
-  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully changed your password!</div>`)
-  $('#message').css('text-align', 'center')
+  $('.nav-message').html(`<div class="alert alert-success" role="alert">You have succesfully changed your password!</div>`)
+  $('.nav-message').css('text-align', 'center')
   $('form').trigger('reset')
   setTimeout(() => {
-    $('#message').html('')
+    $('.nav-message').html('')
   }, 3000
   )
 }
 
 const changePasswordFailure = function () {
-  $('#message').html(`<div class="alert alert-danger" role="alert">Failed to change password</div>`)
-  $('#message').css('text-align', 'center')
+  $('.nav-message').html(`<div class="alert alert-danger" role="alert">Failed to change password</div>`)
+  $('.nav-message').css('text-align', 'center')
   $('form').trigger('reset')
   setTimeout(() => {
-    $('#message').html('')
+    $('.nav-message').html('')
   }, 3000
   )
 }
@@ -105,20 +104,6 @@ const signOutSuccess = function () {
   }, 3000
   )
   store.user = null
-  // $('.create-file').remove()
-  // $('.update-file').remove()
-  // $('.view-file').remove()
-  // $('.delete-file').remove()
-  // $('.change-password').remove()
-  // $('.navbar').remove()
-  // $('.scrollable').remove()
-  // $('.container').remove()
-
-  // const homePageHandlebars = require('../templates/homepage.handlebars')
-  // const homePageHTML = homePageHandlebars()
-
-  // $('body').append(homePageHTML)
-  // // console.log('u signed out')
 }
 const signOutFailure = function () {
   $('#message').html(`<div class="alert alert-danger" role="alert">Failed to sign out!</div>`)
