fb0c7fa6bcf8bf7298e27e548278eaec584274b8
dylan-jannetty <djannetty29@gmail.com>
2018-05-30 13:43:43 -0400

    team name displayed in nav, onLoadPageNoEvent, prepopulated update form, and created signed-in-nav
    ui.js:
      -in signOutSuccess added .update-team reset to try and refresh update form which still isnt
      resolved
      -in signOutSuccess empties nav-team so team name goes away on signout
    
    teamevents.js:
      -created onLoadPageNoEvent so that it gives a welcome message
      -edited onGetTeamNoEvent's promise .then with onGetTeamNoMessageSuccess so
      it'll no longer have a message when the event is called
    
    teamui.js:
      -createTeamSuccess now prepopulates update forms when team is created.
      -if team is made that team name is displayed in nav
      -created a onGetTeamNoMessageSuccess so it'll just get the team with no message
      -onDeleteTeamSuccess add nav-team empty to get rid of team name in nav
      -onUpdateTeam when team is updated it stays prepopulated with new attributes
      and changes name in nav if name is updated
    
    sign-in-nav.handlebars:
      -still needs work

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,16 +1,13 @@
 'use strict'
 const store = require('../store')
 
-const signUpSuccess = function () {
+const signUpSuccess = function (data) {
   $('.nav-message').html(`<div class="alert alert-success" role="alert">You have succesfully signed up</div>`)
   $('.nav-message').css('text-align', 'center')
   $('form').trigger('reset')
-  $('.sign-in').remove()
-  $('.sign-up').remove()
   setTimeout(() => {
     $('.nav-message').html('')
-  }, 3000
-  )
+  }, 3000)
   // $('.dropdown-menu').append('.change-password')
   // $('.dropdown-menu').append('.delete-team')
   // $('.dropdown-menu').append('.create-team')
@@ -29,10 +26,18 @@ const signUpFailure = function () {
 }
 
 const signInSuccess = function (data) {
-  console.log(data)
   $('.nav-message').html(`<div class="alert alert-success" role="alert">You have succesfully signed in!</div>`)
   $('.nav-message').css('text-align', 'center')
   $('form').trigger('reset')
+
+  //
+
+  // const navHandlebars = require('./templates/nav.handlebars')
+  // const navHTML = navHandlebars()
+  // $('.nav').remove()
+  // const signedInNavHandlebars = require('./templates/signed-in-nav.handlebars')
+  // const signedInNavHTML = signedInNavHandlebars()
+  // $('body').prepend(signedInNavHTML)
   // $('.sign-in').remove()
   // $('.sign-up').remove()
 
@@ -61,6 +66,19 @@ const signInSuccess = function (data) {
   }, 3000
   )
   store.user = data.user
+  console.log(store.user)
+  if (store.user.team) {
+    $('.update-team-name').val(store.user.team.name)
+  }
+  if (store.user.team) {
+    $('.update-team-win').val(store.user.team.win)
+  }
+  if (store.user.team) {
+    $('.update-team-loss').val(store.user.team.loss)
+  }
+  if (store.user.team) {
+    $('.nav-team').html(`<div>Team: ${store.user.team.name}</div>`)
+  }
 
   // $('.jumbotron').remove()
   // // console.log(store.user)
@@ -99,10 +117,11 @@ const signOutSuccess = function () {
   $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed out!</div>`)
   $('#message').css('text-align', 'center')
   $('form').trigger('reset')
+  $('.update-team').trigger('reset')
   setTimeout(() => {
     $('#message').html('')
-  }, 3000
-  )
+  }, 3000)
+  $('.nav-team').empty()
   store.user = null
 }
 const signOutFailure = function () {
